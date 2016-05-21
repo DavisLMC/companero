6fbb971246f11be68e6a72fcb59a694983469051
Matt Dresser <m.dres344@gmail.com>
2016-05-21 17:49:47 -0500

    osmdroid testing: hello world style app
    Went through osmdroid's tutorial of how to set up their API for interacting with openstreetmaps. Works extremely easily, very few lines of code to get it up and running.
    
    Things to note
     - Map tiles can be a bit slow to load in comparison to Google Maps, but we're not running on Google's servers either.
     - Font size is very small - this is a known issue : https://goo.gl/w5RttY
     - Works in landscape mode which is a plus!
     - Easily able to add in buttons for zooming. No layout modifications needed.
     - Very high resolution, a lot of detail
    
    This commit runs without issues on the Moto X (Gen 1) | Cyanogen Mod 5.1.1

--- mobile/src/main/java/com/companero/MainActivity.java
+++ mobile/src/main/java/com/companero/MainActivity.java
@@ -1,13 +1,29 @@
 package com.companero;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        moveToMexicoCity(map);
+    }
+
+    private void moveToMexicoCity(MapView map)
+    {
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(19.4326, -99.1332);
+        mapController.setCenter(startPoint);
     }
 }

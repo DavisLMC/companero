808ea358ed9ad5eb5845dc55b0039112125ac9f1
Matt Dresser <m.dres344@gmail.com>
2016-06-09 21:06:31 -0500

    Translation API Integration
    Added Google's translation API to the Gradle build. Translation should now be available to anyone on this branch.

--- mobile/src/main/java/com/companero/MainActivity.java
+++ mobile/src/main/java/com/companero/MainActivity.java
@@ -2,6 +2,7 @@ package com.companero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.api.services.translate.*;
 
 public class MainActivity extends AppCompatActivity {
 
